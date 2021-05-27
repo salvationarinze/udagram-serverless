d6d195a7d8d7921653a395392cbf237b363cb636
Salvation Arinze <modernbabbage@gmail.com>
2021-05-27 12:53:29 +0100

    Initial commit

---
+++ client/src/config.ts
@@ -0,0 +1,10 @@
+
+const apiId = '1q17piytsb'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+export const authConfig = {
+
+  domain: 'arnz3s.us.auth0.com',            // Auth0 domain
+  clientId: '4f6Gq4Z4PS0sw7NpH79tOQ4kDluX6sGJ',          // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
